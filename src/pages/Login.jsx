4c37a34e026fe76bd4b6a493dd26d0d351f17650
Durk006 <141444511+Durk006@users.noreply.github.com>
2024-10-10 23:27:47 -0600

    Update Login.jsx
    Made login page but need styling.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import react, {useState} from 'react'
 
 const Login = () => {
+
+    const [state,setState] = useState('Sign Up')
+
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+    const [name,setName] = useState('')
+
+    const onSubmitHandler = async (event) => {
+        event.preventDefault()
+    }
+
+
+
   return (
-    <div>
-        
-    </div>
+    <form className=''>
+        <div className=''>
+            <p className=''>{state === 'Sign Up' ? "Create Account" : "Login"}</p>
+            <p>Please {state === 'Sign Up' ? "sign up" : "log in"} to book appointment</p>
+            {
+              state === "Sign Up" && <div className=''>
+                <p>Full Name</p>
+                <input className="" type='text' onChange={(e) => setName(e.target.value)} value={name}  />
+              </div>
+            }
+  
+            <div className=''>
+                <p>Email</p>
+                <input className='' type='email' onChange={(e) => setEmail(e.target.value)} value={email}  />
+            </div>
+            <div className=''>
+                <p>Password</p>
+                <input className='' type='password' onChange={(e) => setPassword(e.target.value)} value={password}  />
+            </div>
+            <button className=''>{state === 'Sign Up' ? "Create Account" : "Login"}</button>
+            {
+                state === 'Sign Up'
+                ? <p>Already have an account? <span onClick={('Login')}className="text-primary underline cursor-pointer">Login here</span></p>
+                : <p>Create an new account? <span onClick={('Sign Up')} className="text-primary underline cursor-pointer">click here</span></p>
+            }
+        </div>
+
+    </form>
   )
 }
 
