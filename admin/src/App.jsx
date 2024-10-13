884a60363f6962502efc11296d047fe9a708d343
Durk006 <141444511+Durk006@users.noreply.github.com>
2024-10-12 22:44:54 -0600

    creating the admin panel
    AdminContext.jsx, AppContext.jsx, DoctorContext.jsx, main.jsx

---
+++ admin/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Login from './pages/AdminLogin'
+
+
+const App = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  )
+}
+
+export default App
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { addDoctor } from '../controllers/adminController.js'
+import { addDoctor, loginAdmin } from '../controllers/adminController.js'
 import upload from '../middlewares/multer.js';
+import authAdmin from '../middlewares/authAdmin.js';
 
 const adminRouter = express.Router()
 //middleware process this image and form data
-adminRouter.post('/add-doctor', upload.single('image'), addDoctor)
+adminRouter.post('/add-doctor',authAdmin, upload.single('image'), addDoctor)
+adminRouter.post('/login',loginAdmin)
 
 export default adminRouter
