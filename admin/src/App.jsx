f9820789024cb655005b88437f777f2a08c9261a
Durk006 <141444511+Durk006@users.noreply.github.com>
2024-10-13 16:05:44 -0600

    Admin error notification
    AdminLogin,jsx, App,jsx,

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Login from './pages/AdminLogin'
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <div>
       <Login />
+      <ToastContainer/>
     </div>
   )
 }
