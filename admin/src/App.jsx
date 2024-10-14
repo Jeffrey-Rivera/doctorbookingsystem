53fc47e18811f17901c5a68ea658aabb7696c099
Durk006 <141444511+Durk006@users.noreply.github.com>
2024-10-13 19:24:27 -0600

    Create the pages in Admin folder
    AddDoctor.jsx, AllAppointments.jsx, AddDoctor.jsx and DoctorList.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,6 +4,10 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AdminContext } from './context/AdminContext';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Admin/Dashboard';
+import AllAppointments from './pages/Admin/AllAppointments';
 
 
 const App = () => {
@@ -14,6 +18,16 @@ const App = () => {
     <div className="bg-[#F8F9Fd]">
       <ToastContainer/>
       <Navbar/>
+      <div className='flex items-start'>
+        <Sidebar/>
+        <Routes>
+          <Route path='/' element={<></>} />
+          <Route path='/admin-dashboard' element={<Dashboard/>} />
+          <Route path='/all-appointments' element={<AllAppointments/>} />
+          <Route path='/add-doctor' element={<AddDoctor/>} />
+          <Route path='/doctor-list' element={<DoctorList/>} />
+        </Routes>
+      </div>
     </div>
   ) : (
     <>
