f3d064d8b0516b254fa59114e362b975f0ea3f1b
Jeffrey-Rivera <jeffreyrivera711@gmail.com>
2024-10-14 15:10:24 -0600

    mmy profile and my appointment display

--- src/pages/MyAppointments.jsx
+++ src/pages/MyAppointments.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 const MyAppointments = () => {
+
+  const { doctors } = useContext(AppContext)
+
   return (
     <div>
-        
+      <p className='pb-3 mt-12 font-medium text-zinc-800 border-b'>My Appointments:</p>
+      <div>
+        {doctors.slice(0, 2).map((item, index) => (
+          <div className='grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b' key={index}>
+            <div>
+              <img className='w-32 bg-pink-100' src={item.image} alt="" />
+            </div>
+            <div className='flex-1 text-sm text-zinc-600'>
+              <p className='text-neutral-800 font-semibold'>{item.name}</p>
+              <p>{item.speciality}</p>
+              <p className='text-neutral-800 font-medium mt-1'>Address</p>
+              <p className='text-xs'>{item.address.line1}</p>
+              <p className='text-xs'>{item.address.line2}</p>
+              <p className='text-xs mt-1'><span className='text-sm text-neutral-800 font-medium'>Date & Time:</span> 1, November, 2024 | 9:30 AM</p>
+            </div>
+            <div></div>
+
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
