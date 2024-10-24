8d8cd144685d202ea8da277b480c6974d7655d79
Jeffrey-Rivera <jeffreyrivera711@gmail.com>
2024-10-24 12:54:11 -0600

    my profile page bg color and cancel button

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -40,13 +40,13 @@ const MyProfile = () => {
 
   return (
     userData && (
-      <div className="max-w-lg mx-auto p-6 flex flex-col gap-4 bg-pink-100 rounded-lg text-sm">
+      <div className="max-w-lg mx-auto p-6 flex flex-col gap-4 bg-pink-50 rounded-lg text-sm">
         <div className="flex items-center gap-4">
           {isEdit ? (
             <label htmlFor="image">
               <div className="inline-block relative cursor-pointer">
                 <img
-                  className="w-36 rounded-full border-4 border-pink-300"
+                  className="w-36 rounded-full border-4 border-pink-200"
                   src={image ? URL.createObjectURL(image) : userData.image}
                   alt=""
                 />
@@ -55,7 +55,7 @@ const MyProfile = () => {
               <input onChange={(e) => setImage(e.target.files[0])} type="file" id="image" hidden />
             </label>
           ) : (
-            <img className="w-36 rounded-full border-4 border-pink-300" src={userData.image} alt="Profile" />
+            <img className="w-36 rounded-full border-4 border-pink-200" src={userData.image} alt="Profile" />
           )}
 
           <div>
@@ -64,14 +64,14 @@ const MyProfile = () => {
                 className="bg-pink-50 text-3xl font-medium max-w-60 mt-4 p-2 rounded"
                 type="text"
                 value={userData.name}
-                onChange={(e) => setUserData((prev) => ({ ...prev, name: e.target.value }))}/>
+                onChange={(e) => setUserData((prev) => ({ ...prev, name: e.target.value }))} />
             ) : (
               <p className="font-medium text-3xl text-neutral-800 mt-4">{userData.name}</p>
             )}
           </div>
         </div>
 
-        <hr className="bg-pink-400 h-[1px] border-none" />
+        <hr className="bg-pink-300 h-[1px] border-none" />
 
         <div className="">
           <p className="underline mt-3 font-semibold">CONTACT INFORMATION</p>
@@ -82,7 +82,7 @@ const MyProfile = () => {
             <p className="font-medium">Phone:</p>
             {isEdit ? (
               <input
-                className="bg-pink-50 max-w-52 p-2 rounded"
+                className="bg-pink-100 max-w-52 p-2 rounded"
                 type="text"
                 value={userData.phone}
                 onChange={(e) => setUserData((prev) => ({ ...prev, phone: e.target.value }))}
@@ -95,7 +95,7 @@ const MyProfile = () => {
             {isEdit ? (
               <div className="flex flex-col gap-2">
                 <input
-                  className="bg-pink-50 p-2 rounded"
+                  className="bg-pink-100 p-2 rounded"
                   onChange={(e) =>
                     setUserData((prev) => ({
                       ...prev,
@@ -104,10 +104,10 @@ const MyProfile = () => {
                   }
                   value={userData.address.line1}
                   type="text"
-                  placeholder="Street Address"/>
+                  placeholder="Street Address" />
 
                 <input
-                  className="bg-pink-50 p-2 rounded"
+                  className="bg-pink-100 p-2 rounded"
                   onChange={(e) =>
                     setUserData((prev) => ({
                       ...prev,
@@ -135,7 +135,7 @@ const MyProfile = () => {
             <p className="font-medium">Gender:</p>
             {isEdit ? (
               <select
-                className="max-w-20 bg-pink-50 p-2 rounded"
+                className="max-w-20 bg-pink-100 p-2 rounded"
                 onChange={(e) => setUserData((prev) => ({ ...prev, gender: e.target.value }))}
                 value={userData.gender}
               >
@@ -149,10 +149,10 @@ const MyProfile = () => {
             <p className="font-medium">Birthday:</p>
             {isEdit ? (
               <input
-                className="w-36 bg-pink-50 p-2 rounded"
+                className="w-36 bg-pink-100 p-2 rounded"
                 type="date"
                 onChange={(e) => setUserData((prev) => ({ ...prev, dob: e.target.value }))}
-                value={userData.dob}/>
+                value={userData.dob} />
             ) : (
               <p className="text-neutral-800 ">{userData.dob}</p>
             )}
@@ -161,14 +161,21 @@ const MyProfile = () => {
 
         <div className="mt-10">
           {isEdit ? (
-            <button
-              className="bg-white border-pink-500 px-8 py-2 rounded-full hover:bg-pink-300 hover:text-white transition-all"
-              onClick={updateUserProfileData}>
-              Save information
-            </button>
+            <>
+              <button
+                className="bg-white border-pink-300 px-8 py-2 rounded-full hover:bg-pink-300 hover:text-white transition-all"
+                onClick={updateUserProfileData}>
+                Save information
+              </button>
+              <button
+                className="bg-white border-pink-300 px-8 py-2 rounded-full hover:bg-red-400 hover:text-white transition-all ml-8"
+                onClick={() => setIsEdit(false)}> {/* Adjust this to your function to go back to the profile page */}
+                Cancel
+              </button>
+            </>
           ) : (
             <button
-              className="bg-white border-pink-500 px-8 py-2 rounded-full hover:bg-pink-300 hover:text-white transition-all"
+              className="bg-white border-pink-300 px-8 py-2 rounded-full hover:bg-pink-300 hover:text-white transition-all"
               onClick={() => setIsEdit(true)}>
               Edit
             </button>
