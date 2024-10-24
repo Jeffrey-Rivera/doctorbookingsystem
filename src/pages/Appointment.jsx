2d2ce550a812c4cd93eab991c4f83d101ad247b8
Durk006 <141444511+Durk006@users.noreply.github.com>
2024-10-24 00:50:25 -0600

    Update Appointment.jsx
    Fixed the color

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -137,30 +137,30 @@ const Appointment = () => {
 
         <div className='flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0'>
           {/* ---Doc Info : name, degree, experience--- */}
-          <p className='flex items-center gap-2 text-2xl font-medium text-gray-900'>
+          <p className='flex items-center gap-2 text-2xl font-medium text-gray-500'>
             {docInfo.name}
             <img className='w-5' src={assets.verified_icon} alt="" />
           </p>
-          <div className='flex items-center gap-2 text-sm mt-1 text-gray-600'>
+          <div className='flex items-center gap-2 text-sm mt-1 text-gray-500'>
             <p>{docInfo.degree} - {docInfo.speciality}</p>
             <button className='py-0.5 px-2 border text-xs rounded-full'>{docInfo.experience}</button>
           </div>
 
           {/* ---Doc About--- */}
           <div>
-            <p className='flex items-center gap-1 text-sm font-medium text-gray-900 mt-3'>
+            <p className='flex items-center gap-1 text-sm font-medium text-gray-500 mt-3'>
               About <img src={assets.info_icon} alt="" />
             </p>
             <p className='text-sm text-gray-500 max-w[700px] mt-1'>{docInfo.about}</p>
           </div>
           <p className='text-gray-500 font-medium mt-4'>
-            Appointment fee: <span className='text-gray-600'>{currencySymbol}{docInfo.fees}</span>
+            Appointment fee: <span className='text-gray-700'>{currencySymbol}{docInfo.fees}</span>
           </p>
         </div>
       </div>
 
         {/* ---Booking Slots-- */}
-        <div className='sm:ml-72 sm:pl-4 mt-4 font-medium text-gray-700'>
+        <div className='sm:ml-72 sm:pl-4 mt-4 font-medium text-gray-500'>
           <p>Booking slots</p>
           <div className='flex gap-3 items-center w-full overflow-x-scroll mt-4'>
             {
