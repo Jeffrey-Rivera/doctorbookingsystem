8d5d30ed3b8d9142d6ca522b9101daae442cecc2
Durk006 <141444511+Durk006@users.noreply.github.com>
2024-10-26 15:33:55 -0600

    Settiing up appointments
    AdminContext.jsx, AllAppointments.jsx, adminController.js, adminRoute.js

--- admin/src/pages/Admin/AllAppointments.jsx
+++ admin/src/pages/Admin/AllAppointments.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { useContext } from 'react'
+import { AdminContext } from '../../context/AdminContext'
+import { useEffect } from 'react'
 
 const AllAppointments = () => {
+
+  const {aToken, appointments, getAllAppointments} = useContext(AdminContext)
+
+  useEffect(() => {
+    if (aToken) {
+      getAllAppointments()
+    }
+  }, [aToken])
+
   return (
-    <div>All Appointments</div>
+    <div>
+      <p>All Appointments</p>
+
+      <div>
+        <div>
+          <p>#</p>
+          <p>Patient</p>
+          <p>Age</p>
+          <p>Date & Time</p>
+          <p>Doctor</p>
+          <p>Fees</p>
+          <p>Actions</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
