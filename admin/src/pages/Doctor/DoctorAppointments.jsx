903b9aa52a977fcb6394b5529d67754d54c7857b
Durk006 <141444511+Durk006@users.noreply.github.com>
2024-11-06 16:52:32 -0700

    Doctor appointments
    DoctorContext.jsx, DoctorAppointments.jsx, doctorController.js, authDoctor.js and doctor.Route.js

--- admin/src/pages/Doctor/DoctorAppointments.jsx
+++ admin/src/pages/Doctor/DoctorAppointments.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect, useContext } from 'react'
+import { DoctorContext } from '../../context/DoctorContext'
 
 export const DoctorAppointments = () => {
+
+  const {dToken, appointments, getAppointments} = useContext(DoctorContext)
+
+  useEffect(() => {
+    if (dToken) {
+      getAppointments()
+    }
+  }, [dToken])
+
   return (
     <div>DoctorAppointments</div>
   )
